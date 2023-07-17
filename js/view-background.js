4fbbbe8383c5385aac5e1a327ca0b769db64a9e9
Benny" Wayne Mattis <37753727+wbmattis2@users.noreply.github.com>
2023-07-17 12:36:33 -0400

    fixed view bg for light bg

--- js/view-background.js
+++ js/view-background.js
@@ -1,18 +1,13 @@
 const toggleViewBackground = (e) => {
   const bgButton = e.target;
-  const lmButton = document.getElementById("light-mode-button");
   const mainStyles = document.getElementsByTagName("main")[0].style;
   let currentlyViewing = bgButton.classList.contains("current");
   if (currentlyViewing) {
-  if (document.getElementById("light-mode-button").classList.contains("current")) {
-    document.body.style.backgroundImage = 'none';
-  }
 	bgButton.classList.remove("current");
 	bgButton.title = "Hide content to view background image";
 	mainStyles.visibility = "visible";
     mainStyles.opacity = "0.8";
   } else {
-    document.body.style.backgroundImage = 'url("images/galaxy-background.png")';
 	bgButton.classList.add("current");
 	bgButton.title = "Reveal content over background image";
     mainStyles.opacity = "0";
